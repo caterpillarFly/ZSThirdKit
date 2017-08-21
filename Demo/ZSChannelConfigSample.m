1916aa45d595382fdf72bdce3b3784d573c4483a
zs <zhaosheng_yy@migu.cn>
2017-08-21 17:04:48 +0800

    隐藏appkey等信息

--- Demo/ZSChannelConfigSample.m
+++ Demo/ZSChannelConfigSample.m
@@ -12,11 +12,11 @@
 
 - (NSDictionary *)channelInfoWithType:(ZSChannelType)channelType
 {
-    NSDictionary *qqinfo = @{@"appKey"      :   @"1105787459"};
-    NSDictionary *wxinfo = @{@"appKey"      :   @"wxf6d4fc0792ef3783",
-                             @"appSecert"   :   @"3a9523bcc8a35d664fac915796ce84ec"};
-    NSDictionary *wbinfo = @{@"appKey"      :   @"1843267010",
-                             @"appSecert"   :   @"b2f5b2b661babaa3c01b57312decffd7",
+    NSDictionary *qqinfo = @{@"appKey"      :   @"xxxxxxx"};
+    NSDictionary *wxinfo = @{@"appKey"      :   @"xxxxxxxx",
+                             @"appSecert"   :   @"xxxxxxxxx"};
+    NSDictionary *wbinfo = @{@"appKey"      :   @"xxxxxxxxx",
+                             @"appSecert"   :   @"xxxxxxxxxx",
                              @"redirectURI" :   @"http://music.10086.cn"};
     
     NSDictionary *channelInfoMap = @{@(ZSChannelTypeWX)      :   wxinfo,
